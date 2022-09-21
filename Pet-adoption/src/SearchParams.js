9a218cff2f5ddf17f0af636689bf6d346b05d4bd
Shubham Bonde <shubhambonde2000@gmail.com>
2022-09-22 01:36:21 +0530

    Added Dropdowns in SearchParams.js

--- Pet-adoption/src/SearchParams.js
+++ Pet-adoption/src/SearchParams.js
@@ -1,8 +1,12 @@
 import { useState } from "react";
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
-  
+  const[breed, setBreed] = useState("");
+  const breeds = ["poodle", "pug", "labrador"]
+
   return (
     <div className="search-params">
       <form>
@@ -14,8 +18,50 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
             placeholder="Location"
           />
-          {location}
         </label>
+        <label htmlFor="animal-dropdown">Animal</label>
+        <select
+          id="animal-dropdown"
+          value={animal}
+          onChange={(e) => {
+            setAnimal(e.target.value);
+            setBreed("");
+          }}
+          onBlur={(e) => {
+            setAnimal(e.target.value);
+            setBreed("");
+          }}
+        >
+          <option />
+          {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+        </select>
+        
+
+        <label htmlFor="breed">Breed</label>
+        <select
+          id="breed"
+          value={breed}
+          onChange={(e) => {
+            // setAnimal(e.target.value);
+            setBreed(e.target.value);
+          }}
+          onBlur={(e) => {
+            // setAnimal(e.target.value);
+            setBreed(e.target.value);
+          }}
+        >
+          <option />
+          {breeds.map((b) => (
+              <option key={b} value={b}>
+                {b}
+              </option>
+            ))}
+        </select>
+
         <button>Submit</button>
       </form>
     </div>
