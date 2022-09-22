4b770802cf394b6caa657995c9a5e3bf4d4f5249
Shubham Bonde <shubhambonde2000@gmail.com>
2022-09-22 22:18:07 +0530

    Stylizing the Pet component

--- Pet-adoption/src/Pet.js
+++ Pet-adoption/src/Pet.js
@@ -3,26 +3,31 @@
     jsx into React.createElement() 
     React.createElement() is an API
 */
-const Pet = (props) => {
+const Pet = ({name, animal, breed, images, location, id}) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
+  
   return    (
     // This is JSX
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet" >
+      <div className="image-container">
+        <img src={`${hero}`} alt={`${animal}`} />
+      </div>
+      <div className="info">
+        <h1>
+          {name}
+        </h1>
+        <h2>
+          {animal}-{breed}-{location}
+        </h2>
+      </div>
+    </a>
   );
 };
 
 
-// this translates into
 
-// const Pet = (props) => {
-//     return React.createElement("div", {}, [
-//         React.createElement("h1",{}, props.name),
-//         React.createElement("h2",{}, props.animal),
-//         React.createElement("h2",{}, props.breed)
-//     ]);
-// };
 
 export default Pet;
